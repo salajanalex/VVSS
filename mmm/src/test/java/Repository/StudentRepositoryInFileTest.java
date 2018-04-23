73a3e3b7590e2b0e68ea5c0791af8e32b658026e
salajanalex <alex_sala96cs@yahoo.com>
2018-04-23 18:58:28 +0300

    comentat o clasa inutila

--- mmm/src/test/java/Repository/StudentRepositoryInFileTest.java
+++ mmm/src/test/java/Repository/StudentRepositoryInFileTest.java
@@ -1,16 +1,16 @@
-package Repository;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class StudentRepositoryInFileTest {
-
-    @Test
-    public void createInstance() {
-    }
-
-    @Test
-    public void writeInstace() {
-    }
-}
+//package Repository;
+////
+////import org.junit.Test;
+////
+////import static org.junit.Assert.*;
+////
+////public class StudentRepositoryInFileTest {
+////
+////    @Test
+////    public void createInstance() {
+////    }
+////
+////    @Test
+////    public void writeInstace() {
+////    }
+////}
